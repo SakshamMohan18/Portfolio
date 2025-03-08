6988b9cba1f0773103547a344ccbfe2497bdf35b
SakshamMohan18 <sakshammohan90@gmail.com>
2025-03-08 06:10:33 +0530

    Successful

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -4,7 +4,6 @@ import myImg from "../../Assets/avatar.svg";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
-  AiOutlineTwitter,
   AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
@@ -19,21 +18,20 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+            "Exploring programming has been an exciting journey, and along the way, I've learned and built meaningful things."🤷‍♂️
               <br />
               <br />I am fluent in classics like
               <i>
-                <b className="purple"> C++, Javascript and Go. </b>
+                <b className="purple"> Java, C, Javascript. </b>
               </i>
               <br />
               <br />
               My field of Interest's are building new &nbsp;
               <i>
-                <b className="purple">Web Technologies and Products </b> and
+                <b className="purple">Web based Products </b> and
                 also in areas related to{" "}
                 <b className="purple">
-                  Blockchain.
+                  MERN Stack.
                 </b>
               </i>
               <br />
@@ -48,7 +46,7 @@ function Home2() {
               </i>
               &nbsp; like
               <i>
-                <b className="purple"> React.js and Next.js</b>
+                <b className="purple"> React.js</b>
               </i>
             </p>
           </Col>
@@ -67,7 +65,7 @@ function Home2() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/soumyajit4419"
+                  href="https://github.com/SakshamMohan18"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -77,17 +75,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://twitter.com/Soumyajit4419"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <AiOutlineTwitter />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.linkedin.com/in/soumyajit4419/"
+                  href="https://www.linkedin.com/in/saksham-mohan-452255265/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -97,7 +85,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/soumyajit4419"
+                  href="https://www.instagram.com/saksham_mohan_"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,11 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import happy from "../../Assets/Projects/happy.png";
+import funkky from "../../Assets/Projects/funkky.png";
+import calfit from "../../Assets/Projects/calfit.png";
+import netflix from "../../Assets/Projects/netflix.png";
+import major from "../../Assets/Projects/Major.png";
 
 function Projects() {
   return (
@@ -23,68 +22,55 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={major}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="A Tool for Lecture assessments as per OBE Policy (Under Development)"
+              description="This device automates post-lecture assessments using the MERN stack and IoT, collecting real-time student responses, aligning them with CLOs, providing instant feedback, and delivering analytics for data-driven improvements, enhancing educational efficiency and teaching effectiveness. 🚀"
+              ghLink="https://github.com/SakshamMohan18"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={calfit}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Calfit"
+              description="Calfit is a personalized meal planner that promotes healthier lifestyles by suggesting meals and tracking calories based on user input. Built with HTML, CSS, JavaScript, Node.js, Express.js, and MongoDB, it delivers personalized, scalable meal plans."
+              ghLink="https://github.com/SakshamMohan18"
+              demoLink="https://www.linkedin.com/posts/saksham-mohan-452255265_mealplanner-calorietracker-healthtech-activity-7282053400534392832-KKBR?utm_source=share&utm_medium=member_desktop&rcm=ACoAAED_oT4BFzoG_3xlemMncfe1D6PFmnltQR8"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={funkky}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="Funkky"
+              description="Funkyy is a vibrant, static web page for an online shoe store, designed with HTML and CSS for a playful user experience. It ensures responsiveness, accessibility, and showcases creativity in front-end development and quirky design."
+              ghLink="https://github.com/SakshamMohan18/SakshamMohan18"
+              demoLink="https://www.linkedin.com/posts/saksham-mohan-452255265_webdevelopment-netflixclone-intership-activity-7116519223421497346-YEj7?utm_source=share&utm_medium=member_desktop&rcm=ACoAAED_oT4BFzoG_3xlemMncfe1D6PFmnltQR8"              
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={happy}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="Happy Pizza"
+              description="Developed a responsive pizza delivery web app using React, featuring dynamic customization, real-time updates, and a seamless user experience. Focused on scalability, accessibility, and performance optimization, showcasing expertise in front-end development and user-centric design. "
+              ghLink="https://github.com/SakshamMohan18/Happy-Pizza"
+              demoLink="https://www.linkedin.com/posts/saksham-mohan-452255265_webdevelopment-html5-css-activity-7229417556485234690-bhHQ?utm_source=share&utm_medium=member_desktop&rcm=ACoAAED_oT4BFzoG_3xlemMncfe1D6PFmnltQR8"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={netflix}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
+              title="Netflix Clone"
+              description="Developed a responsive Netflix clone using React, replicating dynamic features like content browsing, user authentication, and real-time updates. Focused on scalability, performance optimization, and creating an engaging, user-friendly interface for seamless navigation."
+              ghLink="https://github.com/SakshamMohan18/Netflix-Clone"
               // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
             />
           </Col>
         </Row>
